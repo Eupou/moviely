d9fb5347553a789ac2a05a1c5621e6ddd81d8772
Wesley Gomes <wesleywesleygomesgomes@gmail.com>
2024-07-22 10:38:24 -0300

    feat: add filters component

---
+++ src/components/FilterLi.jsx
@@ -0,0 +1,14 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
+export default function FilterLi({ isActive, handleActiveLi, icon, children }) {
+  const selected = isActive ? "bg-slate-200" : null
+  return (
+    <li
+      className={`flex items-center hover:cursor-pointer mt-1 rounded-md p-1 ${selected}`}
+      onClick={handleActiveLi}
+    >
+      <FontAwesomeIcon className="w-4" icon={icon} />
+      <p className="ml-2 font-bold">{children}</p>
+    </li>
+  )
+}
--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,3 +1,43 @@
+"use client"
+import FilterLi from "./FilterLi"
+import {
+  faFlask,
+  faDice,
+  faGhost,
+  faFaceLaughSquint,
+  faHeart,
+} from "@fortawesome/free-solid-svg-icons"
+import { useState } from "react"
+
 export default function Filters() {
-  return <aside className="hidden lg:block  w-[20%]">gêneros</aside>
+  const [activeLi, setActiveLi] = useState(0)
+  const movieGender = [
+    { text: "Todos os gêneros", icon: faDice },
+    { text: "Terror", icon: faGhost },
+    { text: "Comédia", icon: faFaceLaughSquint },
+    { text: "Romance", icon: faHeart },
+    { text: "Sci-fi", icon: faFlask },
+  ]
+
+  return (
+    <aside className="hidden lg:block w-[20%] mt-5">
+      <section className="w-[90%] m-auto">
+        <h1 className="font-bold text-sm">Gêneros</h1>
+        <hr className=" text-slate-200 bg-slate-200 my-2 h-[2px]" />
+        <ul>
+          {movieGender &&
+            movieGender.map((gender, id) => (
+              <FilterLi
+                key={id}
+                isActive={activeLi == id}
+                handleActiveLi={() => setActiveLi(id)}
+                icon={gender.icon}
+              >
+                {gender.text}
+              </FilterLi>
+            ))}
+        </ul>
+      </section>
+    </aside>
+  )
 }
