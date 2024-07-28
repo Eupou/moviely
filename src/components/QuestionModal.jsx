b6fe06cda2f6d761c14fe383bd667147447f0dca
Wesley Gomes <wesleywesleygomesgomes@gmail.com>
2024-07-28 17:06:06 -0300

    refactor: export question modal logic with a custom hook

--- src/components/QuestionModal.jsx
+++ src/components/QuestionModal.jsx
@@ -1,111 +1,27 @@
 "use client"
-import { useRef, useState, useContext } from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faXmark } from "@fortawesome/free-solid-svg-icons"
 import Select from "./Select"
-import DOMPurify from "dompurify"
-import questions from "../questions.json"
-import { ModalContext } from "@/context/ModalContext"
 import TextEditor from "./TextEditor"
-
-const INITIAL_STATS = {
-  SCORE: 5,
-  GENDER: undefined,
-  QUESTION: "",
-}
+import useQuestionModal from "@/hooks/useQuestionModal"
 
 export default function QuestionModal({ setNewQuestion }) {
-  const [questionStats, setQuestionStats] = useState(INITIAL_STATS)
-  const [isGenderEmpty, setIsGenderEmpty] = useState("hidden")
-  const [isQuestionEmpty, setIsQuestionEmpty] = useState("hidden")
-  const dialogRef = useRef(null)
-  const { isModalOpen, setIsModalOpen } = useContext(ModalContext)
-
-  const isEmpty = "text-xs font-bold text-red-600"
-
-  function addMovieGender(e) {
-    setQuestionStats((prevStats) => {
-      return { ...prevStats, GENDER: e.target.value }
-    })
-  }
-
-  function addScore(e) {
-    setQuestionStats((prevStats) => {
-      return { ...prevStats, SCORE: parseInt(e.target.value) }
-    })
-  }
-
-  function addQuestion(text) {
-    let sanitazedText = DOMPurify.sanitize(text)
-    setQuestionStats((prevStats) => {
-      return { ...prevStats, QUESTION: sanitazedText }
-    })
-  }
-
-  function isEditorEmpty(content) {
-    const tempElement = document.createElement("div")
-    tempElement.innerHTML = DOMPurify.sanitize(content)
-    return (
-      tempElement.textContent.trim() === "" &&
-      tempElement.querySelector("img") === null
-    )
-  }
-
-  function postQuestion() {
-    questions.push({
-      id: Math.random(),
-      gender: questionStats.GENDER,
-      isFirstQuestion: false,
-      question: questionStats.QUESTION,
-      score: questionStats.SCORE,
-      answerCount: 0,
-    })
-
-    if (setNewQuestion) {
-      setNewQuestion(questions[questions.length - 1])
-    }
-    closeModal()
-  }
-
-  function checkInputs(e) {
-    e.preventDefault()
-    let emptyField = false
-
-    if (questionStats.GENDER == undefined) {
-      emptyField = true
-      setIsGenderEmpty(isEmpty)
-    } else {
-      setIsGenderEmpty("hidden")
-    }
-
-    if (isEditorEmpty(questionStats.QUESTION)) {
-      console.log("esta como vazio")
-      emptyField = true
-      setIsQuestionEmpty(isEmpty)
-    } else {
-      setIsQuestionEmpty("hidden")
-    }
-
-    if (emptyField == false) {
-      postQuestion()
-      setQuestionStats(INITIAL_STATS)
-    }
-  }
-
-  function closeModal() {
-    setIsModalOpen(false)
-    dialogRef.current.close()
-    document.body.style.overflowY = "scroll"
-  }
-
-  if (isModalOpen) {
-    dialogRef.current.showModal()
-  }
+  const {
+    ref,
+    questionStats,
+    isQuestionEmpty,
+    isGenderEmpty,
+    closeModal,
+    addQuestion,
+    addMovieGender,
+    addScore,
+    checkInputs,
+  } = useQuestionModal(setNewQuestion)
 
   return (
     <dialog
       onClose={closeModal}
-      ref={dialogRef}
+      ref={ref}
       className="z-50 w-full lg:max-w-[60vw] h-full absolute top-0 rounded-lg"
     >
       <form method="dialog">
---
+++ src/hooks/useQuestionModal.js
@@ -0,0 +1,109 @@
+import { useState, useContext, useRef } from "react"
+import DOMPurify from "dompurify"
+import questions from "../questions.json"
+import { ModalContext } from "@/context/ModalContext"
+
+const INITIAL_STATS = {
+  SCORE: 5,
+  GENDER: undefined,
+  QUESTION: "",
+}
+
+export default function useQuestionModal({ setNewQuestion }) {
+  const { isModalOpen, setIsModalOpen } = useContext(ModalContext)
+  const [questionStats, setQuestionStats] = useState(INITIAL_STATS)
+  const [isGenderEmpty, setIsGenderEmpty] = useState("hidden")
+  const [isQuestionEmpty, setIsQuestionEmpty] = useState("hidden")
+  const ref = useRef(null)
+  const isEmpty = "text-xs font-bold text-red-600"
+
+  function addMovieGender(e) {
+    setQuestionStats((prevStats) => {
+      return { ...prevStats, GENDER: e.target.value }
+    })
+  }
+
+  function addScore(e) {
+    setQuestionStats((prevStats) => {
+      return { ...prevStats, SCORE: parseInt(e.target.value) }
+    })
+  }
+
+  function addQuestion(text) {
+    let sanitazedText = DOMPurify.sanitize(text)
+    setQuestionStats((prevStats) => {
+      return { ...prevStats, QUESTION: sanitazedText }
+    })
+  }
+
+  function isEditorEmpty(content) {
+    const tempElement = document.createElement("div")
+    tempElement.innerHTML = DOMPurify.sanitize(content)
+    return (
+      tempElement.textContent.trim() === "" &&
+      tempElement.querySelector("img") === null
+    )
+  }
+
+  function postQuestion() {
+    questions.push({
+      id: Math.random(),
+      gender: questionStats.GENDER,
+      isFirstQuestion: false,
+      question: questionStats.QUESTION,
+      score: questionStats.SCORE,
+      answerCount: 0,
+    })
+
+    if (setNewQuestion) {
+      setNewQuestion(questions[questions.length - 1])
+    }
+    closeModal()
+  }
+
+  function checkInputs(e) {
+    e.preventDefault()
+    let emptyField = false
+
+    if (questionStats.GENDER == undefined) {
+      emptyField = true
+      setIsGenderEmpty(isEmpty)
+    } else {
+      setIsGenderEmpty("hidden")
+    }
+
+    if (isEditorEmpty(questionStats.QUESTION)) {
+      emptyField = true
+      setIsQuestionEmpty(isEmpty)
+    } else {
+      setIsQuestionEmpty("hidden")
+    }
+
+    if (emptyField == false) {
+      postQuestion()
+      setQuestionStats(INITIAL_STATS)
+    }
+  }
+
+  function closeModal() {
+    setIsModalOpen(false)
+    ref.current.close()
+    document.body.style.overflowY = "scroll"
+  }
+
+  if (isModalOpen) {
+    ref.current.showModal()
+  }
+
+  return {
+    questionStats,
+    isGenderEmpty,
+    isQuestionEmpty,
+    ref,
+    addMovieGender,
+    addScore,
+    addQuestion,
+    checkInputs,
+    closeModal,
+  }
+}
